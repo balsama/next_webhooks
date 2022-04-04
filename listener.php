b1af294104970901f3eef9d5fb087716ffcf1208
Adam Balsam <adam.balsam@acquia.com>
2022-04-04 15:47:40 -0400

    Add example validation to listener

--- listener.php
+++ listener.php
@@ -1,4 +1,24 @@
 <?php
-$event = json_decode(file_get_contents("php://input"));
+$secret = 'webhooksecret'; // Change this to match the config on the dispatcher.
+$event = file_get_contents("php://input");
 
-file_put_contents('received.json', print_r($event, true));
+$theirHash = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
+$myHash = 'sha256=' . hash_hmac('sha256', $event, $secret, FALSE);
+
+if ($myHash !== $theirHash) {
+    http_response_code(403);
+    echo 'Unable to validate webhook origin.';
+    exit;
+}
+
+http_response_code(202);
+
+$event = json_decode($event);
+$updated_content = [
+    'content_type' => $event->entity->type[0]->target_id,
+    'uuid' =>  $event->entity->uuid[0]->value,
+    'path_alias' => $event->entity->path[0]->alias,
+];
+
+file_put_contents('received.txt', print_r($updated_content, true), FILE_APPEND);
+exit;
